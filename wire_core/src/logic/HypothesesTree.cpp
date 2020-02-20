bb9c7de0a4f552f7abc43ea9d13538f4cf32f49b
laura <l.s.d.jong@student.tue.nl>
2020-02-20 10:06:15 +0100

    only hypothesis of height setsize-1+ better feedback comments

--- wire_core/src/logic/HypothesesTree.cpp
+++ wire_core/src/logic/HypothesesTree.cpp
@@ -81,7 +81,7 @@ void HypothesisTree::addEvidence(const EvidenceSet& ev_set) {
     //** Propagate all objects, compute association probabilities and add all possible measurement-track assignments
     for(EvidenceSet::const_iterator it_ev = ev_set.begin(); it_ev != ev_set.end(); ++it_ev) {
         ObjectStorage::getInstance().match(**it_ev);
-        std::cout << "Evidence_right: " << *it_ev << std::endl;
+        //std::cout << "Evidence_right: " << *it_ev << std::endl;
     }
 
     t_last_update_ = ev_set.getTimestamp();
@@ -385,41 +385,33 @@ void HypothesisTree::pruneTree(const Time& timestamp) {
 }
 
 void HypothesisTree::pruneClusterwise(int setsize) {
-//    Hypothesis* hyp = root_;
-//    printf("assignments: %i \n", hyp->getAssignmentMatrix()->getNumMeasurements());
-
-//
-//    const Assignment &myassi = hyp->getAssignmentMatrix()->getAssignment(0,0);
-//        std::cout << "Evidence:"<< myassi.getEvidence() << std::endl;
-//        std::cout << "Object:"<< myassi.getTarget() << std::endl;
 
     if (root_->getHeight()>=setsize && ClusterStorage::getInstance().size()>0){ //only clustering when enough datapoints & a cluster is available
 
-        //Find set with the right height
+        // Find hypothesis of time t-setsize
         std::map<int,Hypothesis*> hyp_stack;
-
         hyp_stack[root_->getHeight()]=root_;
-
         while(!hyp_stack.empty()) {
-            //Save hyp with too large height
-            Hypothesis* hyp = hyp_stack.rbegin()->second;
-            //printf("height: %i \n",hyp->getHeight());
 
-            //quit when right set is found
+            //quit when right set of hypothesis from time t-setsize is found
             if (hyp_stack.rbegin()->first < setsize) {
                 //printf("stacksize ends = %i \n", hyp_stack.size());
                 break;
             }
 
-            //remove hyp with too large height
+            //Save and remove hyp that is too old
+            Hypothesis* hyp = hyp_stack.rbegin()->second;
             hyp_stack.erase(prev(hyp_stack.end()));
+            //printf("height: %i \n",hyp->getHeight());
 
-            //Extend set with children of hyp with too large height
+            //Extend set with children of the hyp that is too old
             std::list<Hypothesis*>& children = hyp->getChildHypotheses();
             if (!children.empty()) {
                 for (const auto it_child: children) {
+                    if (it_child->getHeight()>=setsize-1) {
                         //printf("add height: %i \n", it_child->getHeight());
-                        hyp_stack[it_child->getHeight()]=it_child;
+                        hyp_stack[it_child->getHeight()] = it_child;
+                    }
                 }
             }
 
@@ -429,33 +421,38 @@ void HypothesisTree::pruneClusterwise(int setsize) {
         for (int n_cluster=0; n_cluster<ClusterStorage::getInstance().size(); n_cluster++) {
 
             //find seed
-            const Evidence *clusterev = ClusterStorage::getInstance().getEvidence(n_cluster, setsize-1);
-            //std::cout << "Cluster seed new:" << clusterev->getAdress() << std::endl;
-
+            const Evidence* clusterev = ClusterStorage::getInstance().getEvidence(n_cluster, setsize-1);
+            std::cout << "Cluster seed new:" << clusterev->getAdress() << std::endl;
 
             //todo:reverse naar van hoog naar laag; loop naar alleen children height 4
             //for all hyps
             for(const auto hyps : hyp_stack){
-                printf("height: %i with prob: %f \n",hyps.second->getHeight(),hyps.second->getProbability());
+                printf("   hyp height: %i with prob: %f \n",hyps.second->getHeight(),hyps.second->getProbability());
 
                 //for all evidences in the hypothesis
                 for (int k = 0; k<hyps.second->getAssignmentMatrix()->getNumMeasurements();k++) {
+                    const Assignment &ev_ass = hyps.second->getAssignmentMatrix()->getAssignment(k, 0);
+                    if (ev_ass.getEvidence() == clusterev->getAdress()) {
+                        printf("      hoera! Now get objects\n");
 
-                    //and all assignments
-                    for (int l = 0; l<hyps.second->getAssignmentMatrix()->getNumAssignments(k);l++) {
-                        const Assignment &myassi = hyps.second->getAssignmentMatrix()->getAssignment(k, l);
-                        //std::cout << "Evidence:" << myassi.getEvidence() << std::endl;
-                        //std::cout << "Object:"<< myassi.getTarget() << std::endl;
+                        //all objects assigned to this evidence
+                        for (int l = 0; l<hyps.second->getAssignmentMatrix()->getNumAssignments(k);l++) {
+                            const Assignment &myassi = hyps.second->getAssignmentMatrix()->getAssignment(k, l);
+                            //std::cout << "Evidence:" << myassi.getEvidence() << std::endl;
+                            //std::cout << "Object:"<< myassi.getTarget() << std::endl;
 
-                        // First
-                        if (myassi.getEvidence() == clusterev->getAdress()) {
+                            // First, determine wich objects the seed can be referring to
                             if (myassi.getTarget()) {
-                                printf("hoera!\n");
-                                std::cout << "Object:" << myassi.getTarget() << " with "<<myassi.getTarget()->getID()<<std::endl;
+                                std::cout << "       Object:" << myassi.getTarget() << " with "<<myassi.getTarget()->getID()<<std::endl;
+                                // in candidatenset
                             }
+
                         }
                     }
+
                 }
+                // if candidatenset, doorsturen naar spoeling
+
             }
 
             //check if a child obays all
