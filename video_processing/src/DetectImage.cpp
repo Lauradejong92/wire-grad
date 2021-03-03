454e29a44f183b0fdaffdbe1fccec81603dcaf64
laura <l.s.d.jong@student.tue.nl>
2021-03-03 13:21:00 +0100

    Publishing to ROS topic in pixels

--- video_processing/src/DetectImage.cpp
+++ video_processing/src/DetectImage.cpp
@@ -3,10 +3,18 @@
 #include <vector>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+#include "wire_msgs/WorldEvidence.h"
+#include "wire_msgs/ObjectEvidence.h"
+#include "problib/conversions.h"
+
 using namespace std;
 using namespace cv;
 
-// Setup SimpleBlobDetector parameters.
+ros::Publisher world_evidence_publisher_;
 
 Mat blackFilter(const Mat& src)
 {
@@ -97,10 +105,6 @@ void detectBlobs(Mat im)
     detector->detect( image, keypoints);
 #endif
 
-    // Draw detected blobs as red circles.
-    // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
-    // the size of the circle corresponds to the size of blob
-
     Mat im_with_keypoints;
     drawKeypoints( image, keypoints, im_with_keypoints, Scalar(100,100,155), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
@@ -109,7 +113,6 @@ void detectBlobs(Mat im)
     waitKey(3000);
     printf("Keypoints found: %lu \n",keypoints.size());
 }
-
 
 void plotCircles(Mat image, vector<Vec3f> y, vector<Vec3f> r,vector<Vec3f> b,vector<Vec3f> z )
 {
@@ -166,10 +169,80 @@ void storingData(vector<Vec3f> y, vector<Vec3f> r,vector<Vec3f> b,vector<Vec3f> 
     countycount++;
 }
 
+void addEvidence(wire_msgs::WorldEvidence& world_evidence, double x, double y, double z, const std::string& class_label, const std::string& color) {
+    wire_msgs::ObjectEvidence obj_evidence;
 
+    // Set the continuous position property
+    wire_msgs::Property posProp;
+    posProp.attribute = "position";
+
+    // Set position (x,y,z), set the covariance matrix as 0.005*identity_matrix
+    pbl::PDFtoMsg(pbl::Gaussian(pbl::Vector3(x, y, z), pbl::Matrix3(0.0005, 0.0005, 0.0005)), posProp.pdf);
+    obj_evidence.properties.push_back(posProp);
+
+    // Set the discrete class label property
+    wire_msgs::Property classProp;
+    classProp.attribute = "class_label";
+    pbl::PMF classPMF;
+
+    // Probability of the class label is 0.7
+    classPMF.setProbability(class_label, 0.7);
+    pbl::PDFtoMsg(classPMF, classProp.pdf);
+    obj_evidence.properties.push_back(classProp);
+
+    // Set the discrete color property with a probability of 0.9
+    wire_msgs::Property colorProp;
+    colorProp.attribute = "color";
+    pbl::PMF colorPMF;
+
+    // The probability of the detected color is 0.9
+    colorPMF.setProbability(color, 0.1);
+    pbl::PDFtoMsg(colorPMF, colorProp.pdf);
+    obj_evidence.properties.push_back(colorProp);
+
+    // Add all properties to the array
+    world_evidence.object_evidence.push_back(obj_evidence);
+}
+
+void generateEvidence(vector<Vec3f> y, vector<Vec3f> r,vector<Vec3f> b,vector<Vec3f> z){
+    // Create world evidence message
+    wire_msgs::WorldEvidence world_evidence;
+
+    // Set header
+    world_evidence.header.stamp = ros::Time::now();
+    world_evidence.header.frame_id = "/map";
+
+    // Add evidence
+    for(size_t i=0; i<y.size(); i++) {
+        addEvidence(world_evidence, y[i][0], y[i][1], 0, "disc", "yellow");
+    }
+    for(size_t i=0; i<r.size(); i++) {
+//        addEvidence(world_evidence, r[i][0], r[i][1], 0, "disc", "red");
+    }
+    for(size_t i=0; i<b.size(); i++) {
+//        addEvidence(world_evidence, b[i][0], b[i][1], 0, "disc", "blue");
+    }
+    for(size_t i=0; i<z.size(); i++) {
+//        addEvidence(world_evidence, z[i][0], z[i][1], 0, "disc", "black");
+    }
+
+    world_evidence_publisher_.publish(world_evidence);
+    ROS_INFO("Published world evidence with size %d", world_evidence.object_evidence.size());
+}
 
 int main(int argc, char** argv )
 {
+//    ros::init(argc, argv, "talker");
+//    ros::NodeHandle nh;
+//    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+// Initialize ros and create node handle
+    ros::init(argc,argv,"dummy_evidence_publisher");
+    ros::NodeHandle nh;
+
+    // Subscriber/publisher
+    world_evidence_publisher_ = nh.advertise<wire_msgs::WorldEvidence>("/world_evidence", 100);
+
     //clear old storage file
     std::ofstream myfile;
     myfile.open("/home/laura/Documents/Data_collection/soccer.m");
@@ -187,10 +260,11 @@ int main(int argc, char** argv )
     //get the frames rate of the video
     double fps = cap.get(CAP_PROP_FPS);
     cout << "Frames per seconds : " << fps << endl;
+    ros::Rate ros_r(fps);
 
     String window_name = "video";
 
-    namedWindow(window_name, WINDOW_NORMAL); //create a window
+    //namedWindow(window_name, WINDOW_NORMAL); //create a window
 
     while (true)
     {
@@ -222,10 +296,12 @@ int main(int argc, char** argv )
         //printf("black circles found: %lu \n",z.size());
 
         //show the frame in the created window
-        plotCircles(image,y,r,b,z);
+        //(image,y,r,b,z);
         //imshow(window_name, blueOnly);
 
-        storingData(y,r,b,z);
+        //storingData(y,r,b,z);
+        generateEvidence(y,r,b,z);
+        ros_r.sleep();
 
 
         //If any key is not pressed withing 10 ms, continue the loop
@@ -235,53 +311,5 @@ int main(int argc, char** argv )
             break;
         }
     }
-
-    return 0;
-
-
-    String folderpath = "/home/laura/Pictures/Soccer/*.png";
-    vector<String> filenames;
-    cv::glob(folderpath, filenames);
-
-//    for (size_t i=0; i<filenames.size(); i++)
-//    {
-//        printf("----------\n Image %lu \n",i+1);
-//
-//        ////Basis:
-//        Mat image = imread(filenames[i]);
-//        //imshow("image", image);
-//        //waitKey(1000);
-//
-//        //// Kleuren:
-////        Mat yellowOnly = colorFilter(image, yellow);
-////        Mat redOnly = colorFilter(image, red);
-////        Mat blueOnly = colorFilter(image, blue);
-////        Mat blackOnly = colorFilter(image, black);//blackFilter(image);
-////    imshow("blackOnly", blackOnly);
-////    waitKey(3000);
-//
-//        //// Detect blobs:
-////        detectBlobs(yellowOnly);
-////        detectBlobs(redOnly);
-////        detectBlobs(blueOnly);
-////        detectBlobs(blackOnly);
-//
-//        //// Detect circles:
-//        //detectCirlces(image);
-////        vector<Vec3f> y = detectCircles(yellowOnly);
-////        printf("yellow circles found: %lu \n",y.size());
-////        vector<Vec3f> r =detectCircles(redOnly);
-////        printf("red circles found: %lu \n",r.size());
-////        vector<Vec3f> b =detectCircles(blueOnly);
-////        printf("blue circles found: %lu \n",b.size());
-////        vector<Vec3f> z =detectCircles(blackOnly);
-////        printf("black circles found: %lu \n",z.size());
-////
-////        plotCircles(image,y,r,b,z);
-//    }
-
-    // detect squares after filtering...
-    printf("done\n");
-
     return 0;
 }
